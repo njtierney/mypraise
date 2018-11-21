2eab9c48a17f823ba7403223797cd347595cbe2b
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-11-21 16:15:50 +1100

    adds glue as a dependency and uses nice features of glue in praise

--- R/praise.R
+++ R/praise.R
@@ -1,12 +1,18 @@
 #' @title Deliver Praise
 #' @description Let's deliver some praise!
 #' @param name The name of the person to praise
+#' @param adjective describe the tone of the praise for this specific sentence structure.
 #' @param punctuation The tone of the praise
 #' @examples
-#'
+#' praise(name = "Miles",
+#'        adjective = "Amazing",
+#'        punctuation = "!")
 #' @export
-praise <- function(name = "Nick", punctuation = "!"){
+praise <- function(name = "Nick",
+                   adjective = "Best",
+                   punctuation = "!"){
 
-  paste0("You're the best, ", name, punctuation)
+  glue::glue("You're the {adjective}, {name}{punctuation}")
 
 }
+
